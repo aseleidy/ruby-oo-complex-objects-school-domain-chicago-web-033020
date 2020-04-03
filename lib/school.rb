b918c4622badf95f26fc8cccc68b2d9232377296
Ashley Etemadi <ash.etemadi@gmail.com>
2020-04-03 18:51:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,6 @@
 # code here!
 class School
-  attr_reader :name 
+  attr_reader :name, :grade  
   attr_accessor :roster 
   
   def initialize(name)
@@ -9,6 +9,7 @@ class School
   end
   
   def add_student(name, grade)
-    
+    @name = name 
+    @grade = grade 
   end
 end 
